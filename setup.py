b6394e28ed60917219a47a1804ac98cb6ead6045
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-10-02 18:37:19 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup_kwargs = setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
     ],
 )
